6e29d6221c12733b05c4d3d019c836bc30df1e30
Levi Porter <levi@levibrooke.com>
2018-11-12 23:35:14 -0800

    Fix frontmatter in template

--- src/templates/post.js
+++ src/templates/post.js
@@ -3,13 +3,13 @@ import { graphql } from 'gatsby';
 
 export default function Template({ data }) {
   const { markdownRemark } = data;
-  const { frontMatter, html } = markdownRemark;
+  const { frontmatter, html } = markdownRemark;
 
   return (
     <div className="blog-post-container">
       <div className="blog-post">
-        <h1>{frontMatter.title}</h1>
-        <h2>{frontMatter.date}</h2>
+        <h1>{frontmatter.title}</h1>
+        <h2>{frontmatter.date}</h2>
         <div
           className="blog-post-content"
           dangerouslySetInnerHTML={{ __html: html }}
